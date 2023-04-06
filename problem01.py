66fb59cfbeb345bfdfa9d2ebe9d50689ba12f24a
PramodEshan <pramodeshanhettigoda@gmail.com>
2023-04-06 19:31:11 +0530

    problem01 completed

---
+++ problem01.py
@@ -0,0 +1,17 @@
+# problem 01
+
+# part a
+word = 'English Academy'
+print(word)  # print English Academy
+# i convert it to lower letters
+print(word.lower())
+# ii convert it to uppercase letters
+print(word.upper())
+# iii check string is lowercase
+print(word.islower())
+# iv find length of the string
+print(len(word))
+# v finding index of A
+print(word.find('A'))
+# replace the word 'English' with 'Statistics'
+print(word.replace('English', 'Statistics'))
